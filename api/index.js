05185deaafe290ae5e8d98cef3d1d13c32921e3d
Anhisa <anhisa@hotmail.com>
2023-08-16 10:47:18 -0500

    public-api

--- api/index.js
+++ api/index.js
@@ -9,18 +9,18 @@ const {
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {});
-const allowList = ['http://127.0.0.1:5500'];
-const options = {
-  origin: (origin, callback) => {
-    if (allowList.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  },
-};
+// const allowList = ['http://127.0.0.1:5500'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (allowList.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'));
+//     }
+//   },
+// };
 
-app.use(cors(options));
+app.use(cors());
 
 app.use(express.json());
 
