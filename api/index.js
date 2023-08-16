13fb9bb6f82950bfe83581d721b48cb4d1960950
Anhisa <anhisa@hotmail.com>
2023-08-16 11:14:59 -0500

    allow changes

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const {
 } = require('./middlewares/error.handler');
 const app = express();
 const port = process.env.PORT || 3000;
-const allowList = ['http://127.0.0.1:5500'];
+const allowList = ['http://127.0.0.1:5500', 'https://myapi-lemon.vercel.app', 'https://myapi-anhisa.vercel.app'];
 const options = {
     origin: (origin, callback) => {
     if (allowList.includes(origin)) {
