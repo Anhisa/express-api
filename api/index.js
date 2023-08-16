68127c0d9b8c6920ad268209e3c02505bf503998
Anhisa <anhisa@hotmail.com>
2023-08-16 12:17:47 -0500

    public-api

--- api/index.js
+++ api/index.js
@@ -8,19 +8,19 @@ const {
 } = require('./middlewares/error.handler');
 const app = express();
 const port = process.env.PORT || 3000;
-const allowList = ['http://127.0.0.1:5500', 'https://myapi-lemon.vercel.app', 'https://myapi-anhisa.vercel.app', 'https://api.greenstudiodev.com'];
-const options = {
-    origin: (origin, callback) => {
-    if (allowList.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  },
-};
+// const allowList = ['http://127.0.0.1:5500', 'http://localhost:3000','https://myapi-lemon.vercel.app', 'https://myapi-anhisa.vercel.app', 'https://api.greenstudiodev.com'];
+// const options = {
+//     origin: function (origin, callback) {
+//     if (allowList.indexOf(origin) !== -1 ) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'));
+//     }
+//   },
+// };
 
 app.listen(port, () => {});
-app.use(cors(options));
+app.use(cors());
 
 app.use(express.json());
 
